5f0b7f981d99b21c7c4838314a29023eeaa0b7f0
Zak Spence <zakspence@gmail.com>
2017-01-18 11:58:27 -0600

    complete

---
+++ instructions.md
@@ -0,0 +1,2 @@
+Replace all occurances of the string "the" with "a large"
+Set the content of the sonnet div with the new string
---
+++ main.js
@@ -0,0 +1,20 @@
+function tweakSonnet() {
+	var target = document.getElementById('sonnet');
+	console.log(target);
+	var sonnet = target.textContent;
+
+	var charCount = sonnet.length;
+	console.log(charCount);
+
+	var orphansIndex = sonnet.indexOf('orphans');
+	console.log('position of \'orphans\': ', orphansIndex);
+
+	sonnet = sonnet.replace(/winter/gi, 'yuletide');
+	sonnet = sonnet.replace(/the/gi, 'a large');
+	console.log(sonnet);
+
+	target.innerHTML = sonnet;
+}
+
+var elemToHear = document.getElementById('sonnet');
+elemToHear.addEventListener("click", tweakSonnet);
